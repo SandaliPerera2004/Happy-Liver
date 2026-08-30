7ef2c5055d0232625fc453a3a9cb2ea06452b7f7
Shehani Mavindi <shehanimavindi@gmail.com>
2026-08-30 13:29:38 +0530

    Added a theme controller to manage dark mode/light mode synchronization

--- lib/services/theme_controller.dart
+++ lib/services/theme_controller.dart
@@ -3,4 +3,8 @@ import 'package:flutter/material.dart';
 class ThemeController {
   static final ValueNotifier<bool> isDarkMode =
   ValueNotifier<bool>(false);
+
+  static Future<void> setDarkMode(bool value) async {
+    isDarkMode.value = value;
+  }
 }
